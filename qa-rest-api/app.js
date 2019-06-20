be05ab920a455f342165c9c01dc92307519f813d
papageg <greggordon98@gmail.com>
2019-06-19 22:55:50 -0700

    night
    night

---
+++ qa-rest-api/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require("express");
+var app = express();
+
+app.use(function(req,res,next){
+    next();
+})
+
+var port = process.env.Port || 3000;
+
+app.listen(port, function(){
+    console.log("Express server is listening on port", port)
+});
